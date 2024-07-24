c2e7137b0d2e887c5d1e7a4f9ff4c41684772247
Ancam <ancam@webspark.com>
2024-07-24 14:31:52 +0300

    Added app code

---
+++ source/app/src/main/java/dev/pages/hova_map/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package dev.pages.hova_map.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
